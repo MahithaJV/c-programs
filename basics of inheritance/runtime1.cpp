170053a027e6991e6506738023bd27d891ebc895
Mahitha JV <mahithajv495@gmail.com>
2025-04-12 12:18:42 +0530

    Add files via upload

---
+++ basics of inheritance/runtime1.cpp
@@ -0,0 +1,37 @@
+# include<iostream>
+using namespace std;
+class base{
+    public:
+    int b;
+     void setdata(int b){
+        this->b=b;
+    }
+    void show(){
+        cout<<"base is called"<<endl;
+        cout<<"b is:"<<b<<endl;
+    }
+};
+class derived: public base{
+    public:
+    int d;
+    void setdata(int d){
+        this->d=d;
+    }
+    void show(){
+        cout<<"child is called"<<endl;
+        cout<<"d is:"<<d<<endl;
+    }
+
+};
+int main(){
+    base b;
+    b.setdata(2);
+    b.show();//2 base
+    derived d;
+    d.setdata(2);
+    d.show();//2 child
+    base *b1= new derived;
+    ((derived *)b1)->setdata(3);//base 3
+((derived *)b1)->show();
+
+}
